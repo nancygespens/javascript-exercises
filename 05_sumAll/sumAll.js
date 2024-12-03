cc404c0d40b37f9b8e32f2d2531e17ec0c7630ff
Anastasia Telesnitckaya <108212488+nancygespens@users.noreply.github.com>
2024-12-03 15:58:42 +0300

    Finished '05_sumAll'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
-};
+const sumAll = function (min, max) {
+    // Iterate over the range and calculate the sum
+    let sum = 0;
+    
+    for (let i = min; i <= max; i++) {
+        sum += i;
+      }
+      return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
