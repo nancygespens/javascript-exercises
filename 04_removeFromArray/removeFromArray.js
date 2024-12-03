40b3d7f22116a32a9610dd24bf5c235d74839954
Anastasia Telesnitckaya <108212488+nancygespens@users.noreply.github.com>
2024-12-03 14:41:27 +0300

    Finished '04_removeFromArray'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, elementToRemove) {
+
+    const index = arr.indexOf(elementToRemove); 
+  
+    if (index > -1) {
+  
+      arr.splice(index, 1); // Removes one element at the found index
+    }
+    return arr; 
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;
