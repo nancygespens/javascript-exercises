bc5d19f2dd60e49b8b9c6acc2852bdd6bca295e6
Anastasia Telesnitckaya <108212488+nancygespens@users.noreply.github.com>
2024-12-03 14:32:05 +0300

    Finished '03_reverseString'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // Split the string into an array of characters
+    const chars = str.split('');
 
-};
+    // Reverse the array
+    chars.reverse();
 
+    // Join the array back into a string
+    return chars.join('');
+}
 // Do not edit below this line
 module.exports = reverseString;
